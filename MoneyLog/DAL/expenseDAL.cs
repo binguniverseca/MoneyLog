0ac2dcaa19f3e0bc25092e2b6d8f7c20adab9bd5
yukiyuli <yukiyuli6688@gmail.com>
2022-10-28 03:28:09 -0400

    Create expenseDAL.cs

---
+++ MoneyLog/DAL/expenseDAL.cs
@@ -0,0 +1,322 @@
+﻿using MoneyLog.BLL;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MoneyLog.DAL
+{
+    class expenseDAL
+    {
+        //Creating STATI String Method for DB Connection
+        static string myconnstrng = ConfigurationManager.ConnectionStrings["connstrng"].ConnectionString;
+
+        #region Select method for expense
+        public DataTable Select()
+        {
+            //Create Sql Connection to connect Databaes
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            //DataTable to hold the data from database
+            DataTable dt = new DataTable();
+
+            try
+            {
+                //Writing the Query to Select all records from database
+                String sql = "SELECT * FROM tbl_expense";
+
+                //Creating SQL Command to Execute Query
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                //SQL Data Adapter to hold the value from database temporarily
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+                //Open Database Connection
+                conn.Open();
+
+                adapter.Fill(dt);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return dt;
+        }
+        #endregion
+        #region Method to Insert expense in database
+        public bool Insert(expenseBLL p)
+        {
+            //Creating Boolean Variable and set its default value to false
+            bool isSuccess = false;
+
+            //Sql Connection for Database
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            try
+            {
+                //SQL Query to insert product into database
+                String sql = "INSERT INTO tbl_expense (type, category, description, amount, added_date) VALUES (@type, @category, @description, @amount, @added_date)";
+
+                //Creating SQL Command to pass the values
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                //Passign the values through parameters
+                cmd.Parameters.AddWithValue("@type", p.type);
+                cmd.Parameters.AddWithValue("@category", p.category);
+                cmd.Parameters.AddWithValue("@description", p.description);
+                cmd.Parameters.AddWithValue("@amount", p.amount);
+                cmd.Parameters.AddWithValue("@added_date", p.added_date);
+                
+
+                //Opening the Database connection
+                conn.Open();
+
+                int rows = cmd.ExecuteNonQuery();
+
+                //If the query is executed successfully then the value of rows will be greater than 0 else it will be less than 0
+                if(rows>0)
+                {
+                    //Query Executed Successfully
+                    isSuccess = true;
+                }
+                else
+                {
+                    //Failed to Execute Query
+                    isSuccess = false;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return isSuccess;
+        }
+        #endregion
+        #region Method to Update expense in Database
+        public bool Update(expenseBLL p)
+        {
+            //create a boolean variable and set its initial value to false
+            bool isSuccess = false;
+
+            //Create SQL Connection for Database
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            try
+            {
+                //SQL Query to Update Data in database
+                String sql = "UPDATE tbl_expense SET type=@type, category=@category, description=@description, amount=@amount, added_date=@added_date WHERE id=@id";
+
+                //Create SQL Cmmand to pass the value to query
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                //Passing the values using parameters and cmd
+                cmd.Parameters.AddWithValue("@type", p.type);
+                cmd.Parameters.AddWithValue("@category", p.category);
+                cmd.Parameters.AddWithValue("@description", p.description);
+                cmd.Parameters.AddWithValue("@amount", p.amount);
+                cmd.Parameters.AddWithValue("@added_date", p.added_date);
+                cmd.Parameters.AddWithValue("@id", p.id);
+
+                //Open the Database connection
+                conn.Open();
+
+                //Create Int Variable to check if the query is executed successfully or not
+                int rows = cmd.ExecuteNonQuery();
+
+                //if the query is executed successfully then the value of rows will be greater than 0 else it will be less than zero
+                if(rows>0)
+                {
+                    //Query ExecutedSuccessfully
+                    isSuccess = true;
+                }
+                else
+                {
+                    //Failed to Execute Query
+                    isSuccess = false;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return isSuccess;
+        }
+        #endregion
+        #region Method to Delete expense from Database
+        public bool Delete(expenseBLL p)
+        {
+            //Create Boolean Variable and Set its default value to false
+            bool isSuccess = false;
+
+            //SQL Connection for DB connection
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            try
+            {
+                //Write Query expense from Database
+                String sql = "DELETE FROM tbl_expense WHERE id=@id";
+
+                //Sql Command to Pass the Value
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                //Passing the values using cmd
+                cmd.Parameters.AddWithValue("@id", p.id);
+
+                //Open Database Connection
+                conn.Open();
+
+                int rows = cmd.ExecuteNonQuery();
+                //If the query is executed successfullly then the value of rows will be greated than 0 else it will be less than 0
+                if(rows>0)
+                {
+                    //Query Executed Successfully
+                    isSuccess = true;
+                }
+                else
+                {
+                    //Failed to Execute Query
+                    isSuccess = false;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return isSuccess;
+        }
+        #endregion
+        #region SEARCH Method for expense
+        public DataTable Search(string keywords)
+        {
+            //SQL Connection fro DB Connection
+            SqlConnection conn = new SqlConnection(myconnstrng);
+            //Creating DAtaTable to hold value from dAtabase
+            DataTable dt = new DataTable();
+
+            try
+            {
+                //SQL query to search records
+                string sql = "SELECT * FROM tbl_expense WHERE id LIKE '%" + keywords + "%' OR type LIKE '%" + keywords + "%' OR category LIKE '%" + keywords + "%'";
+                //Sql Command to execute Query
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                //SQL Data Adapter to hold the data from database temporarily
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+                //Open Database Connection
+                conn.Open();
+
+                adapter.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return dt;
+        }
+        #endregion    
+        #region METHOD TO GET record ID BASED ON TYPE
+        public expenseBLL GetProductIDFromName(string TYPE)
+        {
+            //First Create an Object of DeaCust BLL and REturn it
+            expenseBLL p = new expenseBLL();
+
+            //SQL Conection here
+            SqlConnection conn = new SqlConnection(myconnstrng);
+            //Data TAble to Holdthe data temporarily
+            DataTable dt = new DataTable();
+
+            try
+            {
+                //SQL Query to Get id based on Name
+                string sql = "SELECT id FROM tbl_expense WHERE type='" + TYPE + "'";
+                //Create the SQL Data Adapter to Execute the Query
+                SqlDataAdapter adapter = new SqlDataAdapter(sql, conn);
+
+                conn.Open();
+
+                //Passing the CAlue from Adapter to DAtatable
+                adapter.Fill(dt);
+                if (dt.Rows.Count > 0)
+                {
+                    //Pass the value from dt to DeaCustBLL dc
+                    p.id = int.Parse(dt.Rows[0]["id"].ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return p;
+        }
+        #endregion
+        #region DISPLAY RECORDS BASED ON CATEGORIES
+        public DataTable DisplayProductsByCategory(string category)
+        {
+            //Sql Connection First
+            SqlConnection conn = new SqlConnection(myconnstrng);
+
+            DataTable dt = new DataTable();
+
+            try
+            {
+                //SQL Query to Display Product Based on Category
+                string sql = "SELECT * FROM tbl_expense WHERE category='"+category+"'";
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+                //Open Database Connection Here
+                conn.Open();
+
+                adapter.Fill(dt);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return dt;
+        }
+        #endregion
+    }
+}
